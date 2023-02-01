ef194224a1434618a0ec47b1f35c3b585d773654
DuongHoangThach <boduong95@gmail.com>
2023-02-01 20:37:13 +0700

    fix success.js

--- pages/product/success.js
+++ pages/product/success.js
@@ -2,8 +2,8 @@ import Link from 'next/link';
 import { useEffect } from 'react';
 import { BsBagCheckFill } from 'react-icons/bs';
 
-import { useStateContext } from '../context/StateContext';
-import { runFireworks } from '../lib/utils';
+import { useStateContext } from '../../context/StateContext';
+import { runFireworks } from '../../lib/utils';
 
 const Success = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
